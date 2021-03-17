046d11e59455e5d6b569f294d999430a011cc769
Ito21 <rougecoduire@gmail.com>
2021-03-17 17:31:43 +0900

    適切なコミットコメント3

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -14,10 +14,14 @@ class Post extends Model
     * @return array Postモデルリスト
     */
     
-    /**
-    public function index(Post $post)
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $post->get();
+        // updated_atで好順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
-    */
+
 }
